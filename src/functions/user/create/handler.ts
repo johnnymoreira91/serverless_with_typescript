2d26d2fc224e8511d341204187658a6cac147757
Johnny Moreira <jmoreira@engebras.com.br>
2022-05-30 08:30:55 -0300

    add prisma

--- src/functions/user/create/handler.ts
+++ src/functions/user/create/handler.ts
@@ -1,19 +1,50 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
+import { Prisma, User } from '@prisma/client';
+import prisma from '@database/index'
 
-import schema from './schema';
+// import schema from './schema';
 
-const createUser: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+const createUser: ValidatedEventAPIGatewayProxyEvent<User> = async (event) => {
+  const data = JSON.stringify(event.body);
+  const body = JSON.parse(data)
+  const {name, email, password, permissionLevel} = body
+  console.log(body.name)
   try {
-    return formatJSONResponse({
-      statusCode: 200,
-      message: `Hello ${event.body.name}, your email is ${event.body.email}`
-    });
+    const users = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: password,
+        permissionLevel: permissionLevel || 0
+      }
+    })
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        users
+      }),
+    };
+    // return formatJSONResponse({
+    //   statusCode: 200,
+    //   message: `Hello ${event.body.name}, your email is ${event.body.email}`
+    // });
   } catch (error) {
+    console.log(error)
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      if (error.code === 'P2002') {
+        let message = 'There is a unique constraint violation, a new user cannot be created with this email'
+        return {
+          statusCode: 409,
+          body: JSON.stringify('There is a unique constraint violation, a new user cannot be created with this email')
+        }
+      }
+    }
     return formatJSONResponse({
-      statusCode: 500,
-      message: `Error do list users`
+      errorCode: error.code,
+      error_on: error.meta.target
     });
   }
 };
--- src/functions/user/list/handler.ts
+++ src/functions/user/list/handler.ts
@@ -1,19 +1,13 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
+import prisma from '@database/index'
 
 // import schema from './schema';
 
 const listUser: ValidatedEventAPIGatewayProxyEvent<any> = async () => {
-  const users = [
-    {id: 1, name: 'Johnny'},
-    {id: 2, name: 'Amanda'},
-  ]
   try {
-    // return formatJSONResponse({
-    //   statusCode: 200,
-    //   users
-    // });
+    const users = await prisma.user.findMany()
     return {
       statusCode: 201,
       body: JSON.stringify({
@@ -22,8 +16,7 @@ const listUser: ValidatedEventAPIGatewayProxyEvent<any> = async () => {
     };
   } catch (error) {
     return formatJSONResponse({
-      statusCode: 500,
-      message: `Error do list users`
+      message: error.meta
     });
   }
 };
